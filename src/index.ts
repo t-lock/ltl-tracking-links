a726b17d338b977fcba53f3cbd50e85614570801
Tim Wheelock <tlock@armyofbees.com>
2026-03-12 15:37:36 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+type TrackingInfo = {
+  tracking_number: string;
+  carrier_name?: string;
+};
+
+const carrierUrls: Record<string, string> = {
+  "r&l":
+    "https://www.rlcarriers.com/freight/shipping/shipment-tracing?pro=$$$&docType=PRO&source=web",
+  pittohio:
+    "https://pittohio.com/mypittohio/shipping/quicktrace/publictracingresponse/$$$",
+  aaacooper:
+    "https://www.aaacooper.com/pwb/Transit/ProTrackResults.aspx?ProNum=$$$&AllAccounts=true",
+  aduiepyle: "https://aduiepyle.com/resources/shipment-status/?tracking=$$$",
+};
+
+export function getLTLTrackingLink(trackingInfo: TrackingInfo): string | null {
+  if (!trackingInfo.carrier_name) return null;
+
+  const key = trackingInfo.carrier_name.toLowerCase().replace(/\s/g, "");
+
+  for (const [mapKey, url] of Object.entries(carrierUrls)) {
+    if (key.includes(mapKey)) {
+      return url.replace("$$$", trackingInfo.tracking_number);
+    }
+  }
+
+  return null;
+}
